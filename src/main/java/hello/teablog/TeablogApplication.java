1f982015600fc8ddd85944994ca73ea9bd6d8033
taeheoki <42.4.taeheoki@gmail.com>
2023-04-19 21:19:22 +0900

    initial commit

---
+++ src/main/java/hello/teablog/TeablogApplication.java
@@ -0,0 +1,13 @@
+package hello.teablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeablogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/teablog/TeablogApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.teablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeablogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
